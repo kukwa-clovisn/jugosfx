7b54444064227fa58a35226de43de4f1e7a44aba
kukwa-clovisn <kcn.123.com@gmail.com>
2023-02-24 21:11:55 +0100

    last push

---
+++ public/index.js
@@ -0,0 +1,19 @@
+
+
+// working on menu bar
+
+let menuBtn = document.getElementById("menu-btn");
+let menuCloseBtn = document.getElementById("menu-close-btn")
+let menuHeader = document.getElementById("menu-header")
+
+menuBtn.addEventListener('click', () => {
+    menuHeader.classList.add('show')
+    menuBtn.style.display = 'none'
+    menuCloseBtn.style.display = 'flex'
+})
+
+menuCloseBtn.addEventListener('click', () => {
+    menuHeader.classList.remove('show')
+    menuBtn.style.display = 'flex'
+    menuCloseBtn.style.display = 'none'
+})
