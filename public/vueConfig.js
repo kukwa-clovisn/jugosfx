a35cd2159d36f0d4683eebccfdd354ca081dad7d
kukwa-clovisn <kcn.123.com@gmail.com>
2023-02-27 21:04:55 +0100

    working..

--- public/vueConfig.js
+++ public/vueConfig.js
@@ -1,7 +1,12 @@
 
 
 import { createApp } from "vue";
-import { getTestimonials } from "./app.js";
+import { firebaseApp, db } from './firebaseConfig.js';
+
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js";
+
+
+
 
 const vueApp = createApp({
     data() {
@@ -10,19 +15,18 @@ const vueApp = createApp({
         };
     },
     mounted() {
-        myfunction(getTestimonials());
+        getDocs(collection(db, 'Testimonials')).then(res => {
+            res.docs.map(item => {
+                return this.TestimonialsArr.push(item.data());
+            })
+
+
+        }).catch(err => console.log(err))
     },
     methods: {
-        async myfunction(arr) {
-            try {
-                this.TestimonialsArr = arr;
+        myfunction(arr) {
 
-            }
-            catch (err) {
-                console.log(err)
-            }
-
-
+            this.TestimonialsArr = arr;
         }
 
     }
